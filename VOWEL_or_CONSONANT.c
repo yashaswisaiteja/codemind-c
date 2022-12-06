db21870e693e82651bada59916d344258be94345
yashaswisaiteja <119913198+yashaswisaiteja@users.noreply.github.com>
2022-12-06 19:53:33 +0530

    #include<stdio.h> int main() {     char ch;     scanf("%c",&ch);     if(ch=='a' || ch=='e' ||ch=='i' ||ch=='o' ||ch=='u')     {         printf("VOWEL");     }     else if(ch=='A' ||ch=='E' ||ch=='I' ||ch=='O' ||ch=='U')     {         printf("VOWEL");     }     else     {         printf("CONSONANT");     } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if(ch=='a' || ch=='e' ||ch=='i' ||ch=='o' ||ch=='u')
+    {
+        printf("VOWEL");
+    }
+    else if(ch=='A' ||ch=='E' ||ch=='I' ||ch=='O' ||ch=='U')
+    {
+        printf("VOWEL");
+    }
+    else
+    {
+        printf("CONSONANT");
+    }
+}
