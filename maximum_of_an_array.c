31b67d6fdef197b6f1be1d94bccf9843fba1ce55
yashaswisaiteja <119913198+yashaswisaiteja@users.noreply.github.com>
2023-01-08 18:23:47 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,r,m=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     m=a[0];     for(i=0;i<n;i++)     {         if(m<a[i])         {             m=a[i];         }     }     printf("%d",m); }

---
+++ maximum_of_an_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,r,m=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    m=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(m<a[i])
+        {
+            m=a[i];
+        }
+    }
+    printf("%d",m);
+}
