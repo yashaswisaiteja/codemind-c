2ae097b81dd2b2df50a43477acfc1ac1c435b44b
yashaswisaiteja <119913198+yashaswisaiteja@users.noreply.github.com>
2023-05-21 12:31:54 +0530

    #include<stdio.h> int main() {     int n,d,s=0,p=1;     scanf("%d",&n);     for(;n>0;)     {         d=n%10;         s=s+d;         p=p*d;         n=n/10;     }     if(s==p)     {         printf("Spy Number");     }     else     {         printf("Not Spy Number");     } }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,d,s=0,pro=1;
+    int n,d,s=0,p=1;
     scanf("%d",&n);
     for(;n>0;)
     {
         d=n%10;
         s=s+d;
-        pro=pro*d;
+        p=p*d;
         n=n/10;
     }
-    if(s==pro)
+    if(s==p)
     {
         printf("Spy Number");
     }
